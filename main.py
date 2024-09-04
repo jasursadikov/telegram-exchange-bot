a66fdfa23259462afb7bfd81f8eb1a0b79131495
Jasur Sadikov <jasur@sadikoff.com>
2024-09-04 15:39:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
-from telegram.ext import Application, InlineQueryHandler
+from telegram.ext import Application, ContextTypes, InlineQueryHandler
 import logging
 import os
 import requests
+import asyncio
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-EXCHNAGE_API_URL = 'https://api.exchangerate-api.com/v4/latest/{}'
+EXCHANGE_API_URL = 'https://api.exchangerate-api.com/v4/latest/{}'
 TOKEN = os.getenv('TOKEN')
 
 
-async def convert_currency(amount, from_currency, to_currency):
-    response = requests.get(str.format(from_currency, from_currency)).json()
+def sync_convert_currency(amount, from_currency, to_currency):
+    response = requests.get(EXCHANGE_API_URL.format(from_currency)).json()
     rates = response['rates']
     converted_amount = amount * rates[to_currency]
     return round(converted_amount, 2)
@@ -24,7 +25,13 @@ def currency_to_flag(currency_code):
     return flag
 
 
-async def inline_query(update: Update) -> None:
+async def convert_currency(amount, from_currency, to_currency):
+    loop = asyncio.get_running_loop()
+    converted_amount = await loop.run_in_executor(None, sync_convert_currency, amount, from_currency, to_currency)
+    return converted_amount
+
+
+async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE ) -> None:
     query = update.inline_query.query
     if len(query.split()) != 3:
         return
@@ -44,23 +51,20 @@ async def inline_query(update: Update) -> None:
         amount = float(amount)
         converted_amount = await convert_currency(amount, from_currency, to_currency)
         message = f'{from_currency_emoji} {amount:,.2f} {from_currency} \u27A1 {converted_amount:,.2f} {to_currency} {to_currency_emoji}'
-        results = [InlineQueryResultArticle(id='1',title=message,input_message_content=InputTextMessageContent(message))]
+        results = [InlineQueryResultArticle(id='1', title=message, input_message_content=InputTextMessageContent(message))]
         await update.inline_query.answer(results)
     except Exception as e:
         logger.error(e)
 
 
-async def main() -> None:
+def main() -> None:
     logger.info('Exchange Currency bot started!')
-
     application = Application.builder().token(TOKEN).build()
     application.add_handler(InlineQueryHandler(inline_query))
-    application.run_polling()
-    
-    logger.info('Bot is terminated.')
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+    logger.info('Bot is terminated!')
 
 
 if __name__ == '__main__':
-    import asyncio
-
-    asyncio.run(main())
+    main()
